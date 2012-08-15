d399b835d8f5f1cbe011cba29d1cbe01f534fbcc
Anthony Baker <abakerp@gmail.com>
2012-08-15 17:47:40 +0100

    Committing the first version of the WPF NLog implementation sample.

---
+++ AnthonyBaker.Prototypes.NLog/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using NLog;
+ 
+namespace AnthonyBaker.Prototypes.NLog
+{
+    public partial class MainWindow : Window
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LogButton_Click(object sender, RoutedEventArgs e)
+        {
+            logger.Trace("Sample trace message");
+            logger.Debug("Sample debug message");
+            logger.Info("Sample informational message");
+            logger.Warn("Sample warning message");
+            logger.Error("Sample error message");
+            logger.Fatal("Sample fatal error message");
+
+            // alternatively you can call the Log() method 
+            // and pass log level as the parameter.
+            logger.Log(LogLevel.Info, "Sample fatal error message");
+        }
+    }
+}
